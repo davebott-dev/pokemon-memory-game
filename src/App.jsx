691844073a573ea3223a0e93ccc81994df71f32c
= <bottenbergdave@gmail.com>
2024-07-30 20:51:44 -0400

    current score and high school updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const API_URL = "https://pokeapi.co/api/v2/pokedex/1";
+
+function shuffleArray(array) {
+  //add more or less pokemon in the array by changing the array length in the foor loop
+  for (let i = Math.floor(array.length / 30) - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+}
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [clicked, setClicked] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  cards.length = 12;
+
+  const createCards = async () => {
+    const response = await fetch(`${API_URL}`);
+    const data = await response.json();
+
+    let shuffled = shuffleArray(data.pokemon_entries);
+    setCards(shuffled);
+  };
+
+  useEffect(() => {
+    createCards();
+  }, []);
+
   return (
     <div className="pageCont">
       <div className="score">
         <div className="current">
           <div className="text">Current Score:</div>
-          <div className="num">0</div>
-          </div>
-          <div className="high">
+          <div className="num">{currentScore}</div>
+        </div>
+        <div className="high">
           <div className="text">High Score:</div>
-          <div className="num">0</div>
-          </div>
+          <div className="num">{highScore}</div>
+        </div>
       </div>
       <div>
-      <span>Do not click the same Pokemon twice!</span>
+        <span>Do not click the same Pokemon twice!</span>
 
         <div className="gridCont">
-          <div className="card one"></div>
-          <div className="card two"></div>
-          <div className="card three"></div>
-          <div className="card four "></div>
-          <div className="card five"></div>
-          <div className="card six"></div>
-          <div className="card seven"></div>
-          <div className="card eight"></div>
-          <div className="card nine"></div>
-          <div className="card ten"></div>
-          <div className="card eleven"></div>
-          <div className="card twelve"></div>
+          {cards.map((card, index) => (
+            <div
+              className="card"
+              key={index}
+              onClick={() => {
+                createCards();
+                setClicked([...clicked, card.pokemon_species.name]);
+                if (!clicked.includes(card.pokemon_species.name)) {
+                  setCurrentScore((prev) => prev + 1);
+                } else {
+                  setHighScore(currentScore);
+                  setCurrentScore(0);
+                  setClicked([]);
+                }
+              }}
+            >
+              <img
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${card.entry_number}.png`}
+              />
+              <p>{card.pokemon_species.name}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
