5665c5844fdebaa4bf61c447108a207a9279a08b
= <bottenbergdave@gmail.com>
2024-08-01 08:26:09 -0400

    added functionality to the How To button and Difficulty button

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,39 @@ import "./App.css";
 
 const API_URL = "https://pokeapi.co/api/v2/pokedex/1";
 
-
-
 function App() {
   const [cards, setCards] = useState([]);
   const [clicked, setClicked] = useState([]);
   const [currentScore, setCurrentScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
-  const[index, setIndex]= useState(0);
+  const [index, setIndex] = useState(0);
   const [start, setStart] = useState(33);
-  cards.length = 12;
+  const [length, setLength] = useState(9);
 
-
-  const ShowContents = ({index}) => {
+  const ShowContents = ({ index }) => {
     return index == 1 ? (
       <div className="popUp">
         <div className="title">Choose Your Region</div>
         <div className="regionGroup">
           <div className="group">
-            <div className="region" onClick={()=> {
-              setIndex(0);
-              setStart(181);
-            }}>Kanto</div>
-            <div className="region" onClick ={()=> {
-              setIndex(0);
-              setStart(181);
-            }}>Johto</div>
+            <div
+              className="region"
+              onClick={() => {
+                setIndex(0);
+                setStart(181);
+              }}
+            >
+              Kanto
+            </div>
+            <div
+              className="region"
+              onClick={() => {
+                setIndex(0);
+                setStart(181);
+              }}
+            >
+              Johto
+            </div>
             <div className="region">Hoenn</div>
           </div>
           <div className="group">
@@ -44,29 +51,95 @@ function App() {
         </div>
       </div>
     ) : index == 2 ? (
-      <div className="popUp">Choose Your Difficulty</div>
+      <div className="popUp">
+        <div className="title">Choose Your Difficulty</div>
+        <div className="regionGroup">
+          <div className="group">
+            <div
+              className="region"
+              onClick={() => {
+                setIndex(0);
+                setLength(3);
+                createCards();
+                setCurrentScore(0);
+                setClicked([]);
+              }}
+            >
+              Easy
+            </div>
+            <div
+              className="region"
+              onClick={() => {
+                setIndex(0);
+                createCards();
+                setLength(9);
+                setCurrentScore(0);
+                setClicked([]);
+              }}
+            >
+              Medium
+            </div>
+            <div
+              className="region"
+              onClick={() => {
+                setIndex(0);
+                setLength(15);
+                createCards();
+                setCurrentScore(0);
+                setClicked([]);
+              }}
+            >
+              Hard
+            </div>
+          </div>
+        </div>
+      </div>
     ) : index == 3 ? (
-      <div className="popUp">How To Play</div>
+      <div className="popUp">
+        <div className="title">How To Play</div>
+        <div className="regionGroup">
+          The purpose of this game is to click on as many different Pokemon
+          cards in a row. You get one point for each different card clicked. If
+          you click on a card that was already clicked then the score resets.
+          You can also change the region on pokemon display and the difficulty
+          of the game. <p>Enjoy 😆</p>
+        <div className="group">
+        <div
+              className="region"
+              onClick={() => {
+                setIndex(0);
+              }}
+            >
+              Close
+            </div>
+
+
+        </div>
+       
+       
+        </div>
+      </div>
     ) : null;
   };
-//use arr.slice to create a segment of the large array by regions then shuffle it
+
   function shuffleArray(array) {
-    //add more or less pokemon in the array by changing the array length in the foor loop
-    for (let i = start; i > 0; i--) {
+    for (let i = 33; i > 0; i--) {
       let j = Math.floor(Math.random() * (i + 1));
       let temp = array[i];
       array[i] = array[j];
       array[j] = temp;
     }
-    return array;
+    return array.splice(0, length);
   }
-
+//maybe initialize an empty arr and use a for loop to add specific segments based on 
+//region choice then allow the function to shuffle that array each time?
   const createCards = async () => {
     const response = await fetch(`${API_URL}`);
     const data = await response.json();
 
     let shuffled = shuffleArray(data.pokemon_entries);
     setCards(shuffled);
+    
   };
 
   useEffect(() => {
@@ -77,33 +150,45 @@ function App() {
   return (
     <div className="pageCont">
       <div className="header">
-
-      <div className="score">
-        <div className="current">
-          <div className="text">Current Score:</div>
-          <div className="num">{currentScore}</div>
+        <div className="score">
+          <div className="current">
+            <div className="text">Current Score:</div>
+            <div className="num">{currentScore}</div>
+          </div>
+          <div className="high">
+            <div className="text">High Score:</div>
+            <div className="num">{highScore}</div>
+          </div>
         </div>
-        <div className="high">
-          <div className="text">High Score:</div>
-          <div className="num">{highScore}</div>
+        <div className="btnGroup">
+          <button
+            className="btn"
+            onClick={() => {
+              setIndex(1);
+            }}
+          >
+            Choose Region
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setIndex(2);
+            }}
+          >
+            Change Difficulty
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setIndex(3);
+            }}
+          >
+            How To Play
+          </button>
         </div>
-      </div>
-      <div className="btnGroup">
-        <button className="btn" onClick={()=> {
-          setIndex(1);
-        }}>Choose Region</button>
-        <button className="btn" onClick={()=> {
-          setIndex(2);
-        }}>Change Difficulty</button> 
-        <button className="btn" onClick={()=> {
-          setIndex(3);
-        }}>How To Play</button>
-
-      </div>
-
       </div>
       <div>
-        <ShowContents index ={index}/>
+        <ShowContents index={index} />
         <span>Do not click the same Pokemon twice!</span>
 
         <div className="gridCont">
@@ -116,6 +201,24 @@ function App() {
                 setClicked([...clicked, card.pokemon_species.name]);
                 if (!clicked.includes(card.pokemon_species.name)) {
                   setCurrentScore((prev) => prev + 1);
+                  if(currentScore ==9 && cards.length ==3) {
+                    alert('You Win!');
+                    setCurrentScore(0);
+                  setClicked([]);
+                  setHighScore(currentScore);
+                  }
+                  if(currentScore ==15 && cards.length ==9) {
+                    alert('You Win!');
+                    setCurrentScore(0);
+                  setClicked([]);
+                  setHighScore(currentScore);
+                  }
+                  if(currentScore ==21 && cards.length ==15) {
+                    alert('You Win!');
+                    setCurrentScore(0);
+                  setClicked([]);
+                  setHighScore(currentScore);
+                  }
                 } else {
                   setHighScore(currentScore);
                   setCurrentScore(0);
